f1c1a5ae6c3504be24b595b6d7b295b040cac98d
test2512 <prasat2512@gmail.com>
2017-06-15 13:02:42 +0700

    Create aa.php

--- aa.php
+++ aa.php
@@ -1,6 +1,6 @@
 <?
-$strAccessToken = "04xEhe/EUGysyc4sRXFc4eISQ9nySGxyn2cHTh3uY+F6btffebfZdJ2EC2UOLoC6EqHN/XZQv3fLzyDc0MA7g959PKP+h5MTFOEvbb4jUvqIbvm/5/nV7zap542mHBXgR2htQnnOk8k7XUuNSp2OOVyJkyQKxupqlZ5I0qCNTE8=";
-   //"t4dYu+OXU410/rDWhExXMJg90YZE8xueP0VWCCfofcQm+xfTfYOw17UNNMLM1jtcaE7dkt5HnRNBwtpyNtJSz21QMyELyn28U8ctgdg/BOJRBTsc8AxMlhDoi32BZ8uVZ/aFmis97dfE8KKKi5ztlQdB04t89/1O/w1cDnyilFU=";
+//$strAccessToken = "04xEhe/EUGysyc4sRXFc4eISQ9nySGxyn2cHTh3uY+F6btffebfZdJ2EC2UOLoC6EqHN/XZQv3fLzyDc0MA7g959PKP+h5MTFOEvbb4jUvqIbvm/5/nV7zap542mHBXgR2htQnnOk8k7XUuNSp2OOVyJkyQKxupqlZ5I0qCNTE8=";
+$strAccessToken = "t4dYu+OXU410/rDWhExXMJg90YZE8xueP0VWCCfofcQm+xfTfYOw17UNNMLM1jtcaE7dkt5HnRNBwtpyNtJSz21QMyELyn28U8ctgdg/BOJRBTsc8AxMlhDoi32BZ8uVZ/aFmis97dfE8KKKi5ztlQdB04t89/1O/w1cDnyilFU=";
 
 $strUrl = "https://api.line.me/v2/bot/message/push";
 $arrHeader = array();
