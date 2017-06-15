210b068f312815325f609f127ed930d945c2823e
test2512 <prasat2512@gmail.com>
2017-06-15 10:54:32 +0700

    Create aa.php

--- aa.php
+++ aa.php
@@ -3,16 +3,5 @@
 $strAccessToken = "";
 
 $strUrl = "https://api.line.me/v2/bot/message/push";
- 
-$arrHeader = array();
-$arrHeader[] = "Content-Type: application/json";
-$arrHeader[] = "Authorization: Bearer {$strAccessToken}";
- 
-$arrPostData = array();
-$arrPostData['to'] = "Uafb8898da1f464f55adfe6806a532405";//"USER_ID";
-$arrPostData['messages'][0]['type'] = "text";
-$arrPostData['messages'][0]['text'] = "นี้คือการทดสอบ Push Message from Sahapanich";
- 
-echo "11111"; 
-
+echo "2222";
 ?>	  
