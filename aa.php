3f4e8ad2232b16b6c54d727a8f24f31d5df05053
test2512 <prasat2512@gmail.com>
2017-06-15 10:58:56 +0700

    Create aa.php

--- aa.php
+++ aa.php
@@ -5,5 +5,7 @@ $strUrl = "https://api.line.me/v2/bot/message/push";
 $arrHeader = array();
 $arrHeader[] = "Content-Type: application/json";
 $arrHeader[] = "Authorization: Bearer {$strAccessToken}";
-   echo "555";
+$arrPostData = array();
+$arrPostData['to'] = "Uafb8898da1f464f55adfe6806a532405";//"USER_ID";
+   echo "6666";
 ?>	  
