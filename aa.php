18bc38bf4c3a9cfbcd8a903966fa278f3f3b7d60
test2512 <prasat2512@gmail.com>
2017-06-15 11:00:50 +0700

    Create aa.php

--- aa.php
+++ aa.php
@@ -9,5 +9,6 @@ $arrPostData = array();
 $arrPostData['to'] = "Uafb8898da1f464f55adfe6806a532405";//"USER_ID";
 $arrPostData['messages'][0]['type'] = "text";
 $arrPostData['messages'][0]['text'] = "นี้คือการทดสอบ Push Message from Sahapanich";
-   echo "6666";
+$ch = curl_init();
+   echo "777";
 ?>	  
